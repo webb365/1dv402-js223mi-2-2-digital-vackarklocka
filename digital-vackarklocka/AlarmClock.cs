fd1a8dfe66d33467e85752908a72dd98a52ef146
testt <tse@test.se>
2014-10-14 12:10:20 +0200

    classer

---
+++ digital-vackarklocka/AlarmClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace digital_vackarklocka
+{
+    public class AlarmClock
+    {
+
+    }
+}
---
+++ digital-vackarklocka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digital_vackarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
